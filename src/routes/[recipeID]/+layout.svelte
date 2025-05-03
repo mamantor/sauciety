f8708a92b3b2b7f3b94b6c874c8b351eb8609884
emile <antoine.r.david@gmail.com>
2025-05-03 03:31:54 +0200

    feat: split frieze svg in svelte components

--- src/routes/[recipeID]/+layout.svelte
+++ src/routes/[recipeID]/+layout.svelte
@@ -1,51 +1,39 @@
 <script lang="ts">
 	import '../../app.css';
-	import { onMount } from 'svelte';
-	import svgContent from '$lib/friezes/frieze1.svg?raw'; // Adjust the path as needed
+	import { onMount, type Component } from 'svelte';
+	import Frieze1Part1 from '$lib/friezes/Frieze1Part1.svelte';
+	import Frieze1Part2 from '$lib/friezes/Frieze1Part2.svelte';
+	import Frieze1Part3 from '$lib/friezes/Frieze1Part3.svelte';
+	import Frieze1Part4 from '$lib/friezes/Frieze1Part4.svelte';
+	import Frieze1Part5 from '$lib/friezes/Frieze1Part5.svelte';
+	import Frieze1Part6 from '$lib/friezes/Frieze1Part6.svelte';
 
-	export let children;
-	let firstGroupContent = ""; // Separate local reactivity
-	let firstObject = '';
-	const friezeIndex = 0;
+	const friezes: Array<Component> = [
+		Frieze1Part1,
+		Frieze1Part2,
+		Frieze1Part3,
+		Frieze1Part4,
+		Frieze1Part5,
+		Frieze1Part6
+	];
+	let SelectedFrieze: Component;
 
 	onMount(() => {
-		const parser = new DOMParser();
-		const svgDoc = parser.parseFromString(svgContent, 'image/svg+xml');
-		const firstGroup = svgDoc.querySelector('g');
-		if (firstGroup) {
-			firstGroupContent = firstGroup.children[friezeIndex].outerHTML;
-		}
+		const idx = Math.floor(Math.random() * friezes.length);
+		SelectedFrieze = friezes[idx];
 	});
 </script>
 
 <div>
 	<div class="svg-container">
-		<svg
-			version="1.1"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			x="0px"
-			y="0px"
-			viewBox={`0 ${75 * friezeIndex} 750 100`}
-			xml:space="preserve"
-		>
-			{@html firstGroupContent}
-		</svg>
+		{#if SelectedFrieze}
+			<svelte:component this={SelectedFrieze} />
+		{/if}
 	</div>
-	{@render children()}
+	<slot />
 	<div class="svg-container">
-		<svg
-			version="1.1"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			x="0px"
-			y="0px"
-			viewBox={`0 ${75 * friezeIndex} 750 100`}
-			style="enable-background:new 0 0 750 100;"
-			xml:space="preserve"
-		>
-			{@html firstGroupContent}
-		</svg>
+		{#if SelectedFrieze}
+			<svelte:component this={SelectedFrieze} />
+		{/if}
 	</div>
 </div>
-

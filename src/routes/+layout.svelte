b8475d949dabd1db1340a2c942ce3a704f1a0ef9
emile <antoine.r.david@gmail.com>
2026-04-26 21:56:34 +0200

    feat: adapt to prod

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,7 @@
 	import Menu from '$lib/components/menu.svelte';
 	import { afterNavigate } from '$app/navigation';
 	import Header from '$lib/components/Header.svelte';
-	import { signIn } from '@auth/sveltekit/client';
-	let { children, data } = $props();
+	let { children } = $props();
 
 	afterNavigate(() => {
 		// TODO scroll to top
@@ -13,15 +12,6 @@
 
 <div>
 	<Menu />
-	<div>
-		{#if data.session?.user}
-			<p>Logged in as {data.session.user.email ?? data.session.user.name}</p>
-		{:else}
-			<p>Not logged in</p>
-		{/if}
-
-		<button onclick={() => signIn('authentik')}> Sign in with Authentik </button>
-	</div>
 	<Header />
 	{@render children()}
 </div>
